bda136200284f8cde9d98580e0f59cd26af0764c
maxiSandoval <maxisbravo@gmail.com>
2022-12-20 19:09:33 -0300

    First commit

---
+++ src/main/java/HelloSeleniumTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HelloSeleniumTest {
+
+    @Test
+    public void firstTest()
+    {
+        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.quit();
+    }
+}
