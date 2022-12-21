0c475d39b471f6b42586783308db644286a59dc3
maxiSandoval <maxisbravo@gmail.com>
2022-12-20 23:21:39 -0300

    avance

--- src/main/java/HelloSeleniumTest.java
+++ src/main/java/HelloSeleniumTest.java
@@ -7,9 +7,32 @@ public class HelloSeleniumTest {
     @Test
     public void firstTest()
     {
-        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com/");
+        driver.quit();
+    }
+
+    @Test
+    public void Testherokuapp() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        Thread.sleep(1000);
+        driver.quit();
+    }
+
+    @Test
+    public void TestCypress()
+    {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://example.cypress.io/");
+        driver.quit();
+    }
+
+    @Test
+    public void TestHeroKuappHovers()
+    {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://react-shopping-cart-67954.firebaseapp.com/");
         driver.quit();
     }
 }
